d753b05ad381d41151b3ce2c7f68df908cac3d94
earth156 <65011212091@msu.ac.th>
2024-08-11 05:19:54 +0700

    hh

--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import  { router as index } from "./index.js";
+
 
 export const app = express();
-app.use("/", (req, res) => {
-  res.send("Hello World!!!");
-});
+// app.use("/", (req, res) => {
+//   res.send("Hello World!!!");
+// });
+
+app.use("/", index);
